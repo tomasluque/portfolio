47d39e825369419e96244c6dac30c81be004911a
Tomas Gonzalez <tomasluquegonzalez@gmail.com>
2019-08-31 12:30:10 +0200

    added detail + static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def allblogs(request):
+    blog = Blog.objects
+    return render(request, 'blog/allblogs.html', {'blog': blog})
+
+def detail(request, blog_id):
+    detailBlog = get_object_or_404(Blog, pk=blog_id)
+    return render(render, 'blog/detail.html', {
+        'blog': detailBlog
+    })
