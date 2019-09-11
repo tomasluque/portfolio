a53232eba207ca02a324ca563fe70ecd8067c6e2
tomasluque <tomasluquegonzalez@gmail.com>
2019-09-11 20:00:00 +0200

    edited model jobs + added visual

--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
