25604b0e1d78d6c91eb112b8ecca566524c2af1b
tomasluque <tomasluquegonzalez@gmail.com>
2019-09-26 18:21:37 +0200

    added new styling to blog

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,7 +7,7 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls')),
+    path('cinema/', include('blog.urls')),
     path('game/', include('game.urls')),
     path('words/', include('wordCount.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
